81b3081f8e3b535a3dca3bf95f625e5d50ae49f8
ahmed alaa <ahmedtawfiiiq24@gmail.com>
2022-11-23 02:07:18 +0200

    final submit

---
+++ 6842-lab4/6842-7194-lab4/process.hpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Process
+{
+public:
+    int at;     // 1 arrive time
+    int bt;     // 2 burst time
+    int ct;     // 3 completion time
+    int tat;    // 4 turn around time
+    int wt;     // 5 waiting time
+    string pid; // 6 process id
+
+    pair<string, int> output;
+
+    Process() : at(0), bt(0), ct(0), tat(0), wt(0), pid(""), output("", 0) {}
+};
+
+class Processes
+{
+public:
+    string scheduleName;
+    int stopRange;
+    double tatMean;
+    vector<Process> processes;
+    Processes() : scheduleName(""), tatMean(0) {}
+};
+
+void organizeData(Processes &, vector<vector<string>>, int);
+
+void stats(Processes);
+void trace(Processes);
+
+void doFCFS(Processes &);
+
+void doRR(Processes &, int);
+
+void doSPN(Processes &);
+
+void doSRT(Processes &p);
+
+void doHRR(Processes &p);
+
+void doFBq1(Processes &p);
+
+void doFBq2i(Processes &p);
---
+++ 6842-lab4/6842-7194-lab4/tokenizer.hpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool sortColumn(const vector<string> &v1, const vector<string> &v2);
+
+void typeTokenizer(vector<vector<int>> &);
+
+void processesTokenizer(vector<vector<string>> &data, int n);
+
+void displayData(vector<vector<int>> data);
